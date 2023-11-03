660a72a698cfa64117603f2b303aad5c4b948284
PedroHSSodre <pedrosodredev@gmail.com>
2023-11-03 00:37:41 -0300

    feat: added scrollview to wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,12 +1,50 @@
 import React from 'react';
-import {Flex, IFlexProps} from 'native-base';
+import {Flex, IFlexProps, IScrollViewProps, ScrollView} from 'native-base';
+import {ImageBackground} from 'react-native';
+import {type StyleProp, type ViewStyle} from 'react-native';
 
-type WrapperProps = IFlexProps;
+type WrapperProps = {bgImage?: boolean; withScrollView?: boolean} & IFlexProps;
 
 export const Wrapper = (props: WrapperProps) => {
+  const {children, bgImage = false, withScrollView = true, ...rest} = props;
+  if (withScrollView) {
+    return (
+      <ScrollView {...scrollViewProps}>
+        <Flex flexDirection="column" flex={1} w="full" {...rest}>
+          {children}
+          {bgImage ? (
+            <ImageBackground
+              source={require('../assets/images/mountains.png')}
+              style={_imgBackgroundStyle}
+              resizeMode="cover"
+            />
+          ) : null}
+        </Flex>
+      </ScrollView>
+    );
+  }
+
   return (
-    <>
-      <Flex flexDirection="column" flex={1} w="full" {...props} />
-    </>
+    <Flex flexDirection="column" flex={1} w="full" {...rest}>
+      {children}
+      {bgImage ? (
+        <ImageBackground
+          source={require('../assets/images/mountains.png')}
+          style={_imgBackgroundStyle}
+          resizeMode="cover"
+        />
+      ) : null}
+    </Flex>
   );
+};
+
+const scrollViewProps: IScrollViewProps = {
+  contentContainerStyle: {flexGrow: 1},
+};
+
+const _imgBackgroundStyle: StyleProp<ViewStyle> = {
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  zIndex: 1,
 };
