f0c54626a495546fb4f6eea1f2abb6c24fffd41e
PedroHSSodre <pedrosodredev@gmail.com>
2023-11-03 00:36:16 -0300

    feat: hidden statusbar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import {NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider, StatusBar} from 'native-base';
 import React from 'react';
 import {SafeAreaView, ViewStyle} from 'react-native';
+import {RootNavigation} from './src/components/Routes';
 
 function App(): JSX.Element {
   return (
     <NativeBaseProvider>
       <SafeAreaView style={style}>
+        <StatusBar hidden />
         <RootNavigation />
       </SafeAreaView>
     </NativeBaseProvider>
