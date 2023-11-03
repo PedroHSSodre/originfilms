d90d4f93d2e8e1e3e1fd77b193ced38f907e5985
PedroHSSodre <pedrosodredev@gmail.com>
2023-11-03 00:35:59 -0300

    feat: add api option sending TMDB token

---
+++ src/config/api.tsx
@@ -0,0 +1,8 @@
+export const apiOptions = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMzY4ZjZlZWU3M2NkODFhMGJmNDllZjQwODkyYTE3MSIsInN1YiI6IjY1NDNmZTZjZTFhZDc5MDBlYTU4MTEwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VDSvOiwvMdCFaLdcqMJSLiGQzyuNIuGrCekkzTLkS7w',
+  },
+};
