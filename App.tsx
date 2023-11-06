2a094002e36abf361a5fa0ee4f8d92b7ab1bd56f
PedroHSSodre <pedrosodredev@gmail.com>
2023-11-05 22:49:20 -0300

    chore: config react query

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import {SafeAreaView, ViewStyle} from 'react-native';
 import {NativeBaseProvider, StatusBar} from 'native-base';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 import {RootNavigation} from './src/components/Routes';
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
-    <NativeBaseProvider>
-      <SafeAreaView style={style}>
-        <StatusBar hidden />
-        <RootNavigation />
-      </SafeAreaView>
-    </NativeBaseProvider>
+    <QueryClientProvider client={queryClient}>
+      <NativeBaseProvider>
+        <SafeAreaView style={style}>
+          <StatusBar hidden />
+          <RootNavigation />
+        </SafeAreaView>
+      </NativeBaseProvider>
+    </QueryClientProvider>
   );
 }
 
